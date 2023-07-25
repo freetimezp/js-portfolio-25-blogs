2a0226207a4a20e849a851b8ae38283f52b4d03a
freetimezp <kraftukrnet@ukr.net>
2023-07-25 23:19:39 +0300

    05. js-portfolio-25-blogs. Banner Component.

---
+++ app/components/Banners/Banner.tsx
@@ -0,0 +1,43 @@
+"use client";
+import React from 'react';
+
+import Button from '../Button/Button';
+
+interface Props {
+    text: string;
+    button?: boolean;
+    background?: string;
+    shadow?: boolean;
+    color?: string;
+    subtitle?: string;
+}
+
+function Banner({
+    text,
+    button,
+    background = "linear-gradient(124deg, rgba(88,140,235,1) 0%, rgba(186,70,204,1) 100%)",
+    shadow,
+    color,
+    subtitle
+}: Props) {
+    return (
+        <div className='banner'
+            style={{
+                background: background,
+                boxShadow: shadow ? "0px 4px 4px rgba(0, 0, 0, 0.25)" : "none"
+            }}
+        >
+            <p className='banner__text' style={{ color: color }}>
+                {text}
+            </p>
+            <p className='banner__subtitle'>
+                {subtitle}
+            </p>
+            {button && (
+                <Button text="Contact Us" bg="white" padding="1.2rem 2rem" hover="true" />
+            )}
+        </div>
+    );
+};
+
+export default Banner;
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@
 import React from "react";
 
 import Portfolios from "./components/Portfolios/Portfolios";
+import Banner from "./components/Banners/Banner";
 
 export default function Home() {
   return (
     <main className="u-pad-2">
       <Portfolios />
+      <Banner
+        color="white"
+        text="Interested in contact with Me? Send me email.."
+        subtitle="Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem"
+        button
+      />
     </main>
   );
 }
