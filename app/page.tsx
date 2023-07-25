edfec4529db54c7ce73b93e0dc0c827437d2723c
freetimezp <kraftukrnet@ukr.net>
2023-07-25 23:42:21 +0300

    06. js-portfolio-25-blogs. MiniBanner, MiniPortfolios section.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import React from "react";
 
 import Portfolios from "./components/Portfolios/Portfolios";
 import Banner from "./components/Banners/Banner";
+import PortfolioItem from "./components/PortfolioItem/PortfolioItem";
+
+import { portfolios } from "./utils/portfolios";
 
 export default function Home() {
   return (
@@ -14,6 +17,28 @@ export default function Home() {
         subtitle="Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem"
         button
       />
+
+      <div className="mini-portfolios">
+        {portfolios?.slice(-4).map((portfolio) => (
+          <PortfolioItem
+            key={portfolio.id}
+            name={portfolio.name}
+            image={portfolio.image}
+            url={portfolio.url}
+            categories={portfolio.categories}
+          />
+        ))}
+      </div>
+
+      <div className="mini-banner">
+        <Banner
+          background="white"
+          shadow
+          color="#6366f1"
+          text="Let's take Your Happiness Moment in life on My Camera.."
+        />
+      </div>
+
     </main>
   );
 }
