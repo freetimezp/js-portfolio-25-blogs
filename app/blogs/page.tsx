92d64c857d62057e8997eb3c721d29f97e670ff3
freetimezp <kraftukrnet@ukr.net>
2023-07-30 22:48:41 +0300

    12. js-portfolio-25-blogs. Blogs page. Animation.

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import Banner from '../components/Banners/Banner';
 import ContactBanner from './ContactBanner';
@@ -7,20 +8,27 @@ import ContactBanner from './ContactBanner';
 import { blogs } from '../utils/blogs';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { gridItemsVariants, gridVariants } from '../utils/animation';
 
 const BlogsPage = () => {
     const router = useRouter();
 
     return (
         <div className='u-pad-2'>
-            <div className="blogs">
+            <motion.div
+                className="blogs"
+                variants={gridVariants}
+                initial="hidden"
+                animate="visible"
+            >
                 {blogs?.map((blog, i: number) => (
-                    <div
+                    <motion.div
                         className='blog'
                         key={i}
                         onClick={() => {
                             router.push(`/blog/${blog.title?.replace(/:/g, "").split(" ").join("-").toLocaleLowerCase()}`);
                         }}
+                        variants={gridItemsVariants}
                     >
                         <div className='blog__image'>
                             {blog.image && <Image src={blog.image} alt="blog" />}
@@ -28,9 +36,9 @@ const BlogsPage = () => {
                         <h4 className="blog__title">
                             {blog?.title}
                         </h4>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
             <Banner
                 color="white"
                 button
