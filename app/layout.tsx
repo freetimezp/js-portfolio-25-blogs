425a40289e4ca39c5cd02f4d1b89b8a6cd8c58ca
freetimezp <kraftukrnet@ukr.net>
2023-07-31 21:52:38 +0300

    14. js-portfolio-25-blogs. Progress bar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+"use client";
 import React from 'react';
 
 import './styles/main.scss';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { motion, useSpring, useScroll } from 'framer-motion';
 
 import Navigation from './components/Navigation/Navigation';
 
@@ -19,9 +21,17 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const scrollYProgress = useScroll().scrollYProgress;
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 20,
+    restDelta: 0.001
+  });
+
   return (
     <html lang="en">
       <body className={inter.className}>
+        <motion.div className='progress-bar' style={{ scaleX: scaleX }}></motion.div>
         <Navigation />
         {children}
       </body>
